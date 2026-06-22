837acd4b4658229afd0345c40c9e71a34efc98bb
vijayakarenavar <vijaylaxmikarenavar@gmail.com>
2026-06-22 16:24:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/srg_newapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.srg_newapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,15 @@
+// test/widget_test.dart
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:srg_app/main.dart';
+
+
+void main() {
+  testWidgets('SRG App launches successfully', (WidgetTester tester) async {
+    await tester.pumpWidget(const SRGApp());
+    await tester.pumpAndSettle();
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
